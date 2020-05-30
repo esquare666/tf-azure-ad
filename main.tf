ce611294dcb75e4d15829a97ccc0a99f17bc4ad5
esquare666 <elayaraja.kv@gmail.com>
2020-05-30 14:50:04 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,5 @@ provider "azuread" {
 resource "azuread_user" "home" {
   user_principal_name = "home@3es.nz"
   display_name        = "Elan Home"
-  password            = "Password@1234"
   usage_location      = var.usage_location
 }
