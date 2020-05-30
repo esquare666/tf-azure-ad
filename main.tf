0789fed143cb74892fcc5a455ef51e7baa6baf7b
esquare666 <elayaraja.kv@gmail.com>
2020-05-30 15:01:40 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "azuread" {
   version = "~> 0.7.0"
 }
 
-resource "azuread_user" "home" {
+resource "azuread_user" "main" {
   user_principal_name = "home@3es.nz"
   display_name        = "Elan Home"
+  #Var is created in env. export TF_VAR_password="wjwgergnerjgnerjkg@fgjner43"
   password            = var.password
   usage_location      = var.usage_location
 }
