3eb8821fa7a620ac78f9ac8a0ff007da1d06caa6
esquare666 <elayaraja.kv@gmail.com>
2020-05-30 16:01:09 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,16 @@ provider "azuread" {
 }
 
 resource "azuread_user" "main" {
-  count = length(var.ad_home_group_users)
-  user_principal_name = "${var.ad_home_group_users[count.index].uid}@3es.nz"
-  display_name = var.ad_home_group_users[count.index].dname
+  count = length(var.ad_users)
+  user_principal_name = "${var.ad_users[count.index].uid}@3es.nz"
+  display_name = var.ad_users[count.index].dname
   #Var is created in env. export TF_VAR_password="wjwgergnerjgnerjkg@fgjner43"
   password            = var.password
   force_password_change = false
   usage_location      = var.usage_location
+}
+
+resource "azuread_group" "main" {
+  count = length(var.ad_groups)
+  name  = var.ad_groups[count.index]
 }
