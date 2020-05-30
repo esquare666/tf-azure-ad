b4fd22cd21710621890fbc97b38ca9574643ebb2
esquare666 <elayaraja.kv@gmail.com>
2020-05-30 15:37:38 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,10 @@ provider "azuread" {
 }
 
 resource "azuread_user" "main" {
-  user_principal_name = "home@3es.nz"
-  display_name        = "Elan Home"
+  count = length(var.ad_home_group_users)
+  cache_level = 
+  user_principal_name = "${var.ad_home_group_users[count.index].uid}@3es.nz"
+  display_name = "${var.ad_home_group_users[count.index].dname}"
   #Var is created in env. export TF_VAR_password="wjwgergnerjgnerjkg@fgjner43"
   password            = var.password
   force_password_change = false
